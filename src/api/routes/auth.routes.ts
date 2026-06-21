e72333bcc8e4d0f6f0c857ba3308d5a4da8334a8
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-06-22 00:00:40 +0600

    6th commit

---
+++ src/api/routes/auth.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express"
+
+
+const router = Router()
+
+router.post("/signUp")
+router.post("/login")
+router.get("/me")
+router.put("/update/:id")
+router.delete("/delete/:id")
