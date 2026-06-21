5dacdb8fdea21ae7e3dc793a3e923244aa067765
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-06-21 21:08:10 +0600

    3rd commit

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,5 +5,31 @@ export const sql = neon(config.database_url)
 
 
 export const initDB=async()=>{
+
+   await sql`
+    CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(75) NOT NULL,
+    email VARCHAR(25) UNIQUE NOT NULL,
+     
+    password_hash TEXT NOT NULL,
+
+    age INT NOT NULL,
+    role VARCHAR(25) NOT NULL DEFAULT "user",
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+      updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+    )
+    `
+     await sql`
+    CREATE TABLE IF NOT EXISTS orders(
+    id SERIAL PRIMARY KEY,
+    customerId INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    quantity INT NOT NULL CHECK(quantity>0),
+    food TEXT NOT NULL,
+    price NUMERIC(10,2) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+      updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+    )
+    `
     console.log("database conneected successfully!")
 }
